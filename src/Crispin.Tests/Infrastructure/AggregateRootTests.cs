8deb6e42075f3c92bf2d8afb747b321902bd1dfc
andy <andy@andydote.co.uk>
2017-08-07 22:04:39 +0300

    aggregateRoot: support saving events

---
+++ src/Crispin.Tests/Infrastructure/AggregateRootTests.cs
@@ -0,0 +1,68 @@
+﻿using Crispin.Infrastructure;
+using Shouldly;
+using Xunit;
+
+namespace Crispin.Tests.Infrastructure
+{
+	public class AggregateRootTests
+	{
+		private readonly TestAggregate _aggregate;
+
+		public AggregateRootTests()
+		{
+			_aggregate = new TestAggregate();
+		}
+
+		[Fact]
+		public void When_there_are_no_events_to_be_saved()
+		{
+			_aggregate.GetPendingEvents().ShouldBeEmpty();
+		}
+
+		[Fact]
+		public void When_an_event_is_applied()
+		{
+			var testEvent = new TestEventOne();
+			_aggregate.Raise(testEvent);
+
+			_aggregate.GetPendingEvents().ShouldHaveSingleItem().ShouldBe(testEvent);
+		}
+
+		[Fact]
+		public void When_an_applied_event_has_been_saved()
+		{
+			_aggregate.Raise(new TestEventOne());
+			_aggregate.ClearPendingEvents();
+
+			_aggregate.GetPendingEvents().ShouldBeEmpty();
+		}
+
+		[Fact]
+		public void When_multiple_events_are_saved()
+		{
+			var events = new[]
+			{
+				new TestEventOne(),
+				new TestEventOne(),
+				new TestEventOne()
+			};
+
+			foreach (var e in events)
+				_aggregate.Raise(e);
+
+			_aggregate.GetPendingEvents().ShouldBe(events);
+		}
+
+		private class TestAggregate : AggregateRoot
+		{
+			public TestAggregate()
+			{
+				Register<TestEventOne>(e => { });
+			}
+
+			public void Raise(object e) => ApplyEvent(e);
+		}
+
+		private class TestEventOne {}
+	}
+}
--- src/Crispin/Infrastructure/AggregateRoot.cs
+++ src/Crispin/Infrastructure/AggregateRoot.cs
@@ -3,16 +3,33 @@ using System.Collections.Generic;
 
 namespace Crispin.Infrastructure
 {
-	public class AggregateRoot
+	public abstract class AggregateRoot
 	{
 		private readonly Dictionary<Type, Action<object>> _handlers;
+		private readonly List<object> _pendingEvents;
 
 		protected AggregateRoot()
 		{
 			_handlers = new Dictionary<Type, Action<object>>();
+			_pendingEvents = new List<object>();
 		}
 
 		protected void Register<TEvent>(Action<TEvent> handler) => _handlers.Add(typeof(TEvent), e => handler((TEvent)e));
-		protected void ApplyEvent<TEvent>(TEvent @event) => _handlers[@event.GetType()](@event);
+
+		protected void ApplyEvent<TEvent>(TEvent @event)
+		{
+			_pendingEvents.Add(@event);
+			_handlers[@event.GetType()](@event);
+		}
+
+		public IEnumerable<object> GetPendingEvents()
+		{
+			return _pendingEvents;
+		}
+
+		public void ClearPendingEvents()
+		{
+			_pendingEvents.Clear();
+		}
 	}
 }
