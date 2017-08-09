aa96f78b9c40b08e5ba41d64ec95d5f07df3835e
andy <andy@andydote.co.uk>
2017-08-09 17:16:44 +0300

    build: update to run tests

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ dotnet restore
 dotnet build ./src/$NAME --configuration $MODE
 dotnet build ./src/$NAME.Tests --configuration $MODE
 
-dotnet test ./src/$NAME.Tests --configuration $MODE
+dotnet test ./src/$NAME.Tests/$NAME.Tests.csproj --configuration $MODE
 
 mkdir -p ./build/deploy
 dotnet pack ./src/$NAME --configuration $MODE --output ../../build/deploy
