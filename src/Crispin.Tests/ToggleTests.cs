a946911be785eceb4b051cbe4faadb79f169ef8e
andy <andy@andydote.co.uk>
2017-08-09 17:25:27 +0300

    toggle: add tests for toggle load

---
+++ src/Crispin.Tests/ToggleTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Crispin.Events;
+using Shouldly;
+using Xunit;
+
+namespace Crispin.Tests
+{
+	public class ToggleTests
+	{
+		[Fact]
+		public void When_loading_from_an_event_stream()
+		{
+			var toggleCreated = new ToggleCreated(Guid.NewGuid(), "toggle name", "toggle desc");
+			var toggle = Toggle.LoadFrom(new object[] { toggleCreated });
+			
+			toggle.ShouldSatisfyAllConditions(
+				() => toggle.ID.ShouldBe(toggleCreated.ID),
+				() => toggle.Name.ShouldBe(toggleCreated.Name),
+				() => toggle.Description.ShouldBe(toggleCreated.Description)
+			);
+		}
+	}
+}
