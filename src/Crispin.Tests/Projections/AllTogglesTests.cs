333dfc031b6aa29639d569983764c8d4cfaeb4b1
andy <andy@andydote.co.uk>
2017-08-11 21:47:45 +0300

    projections: implement basic allToggles projection

---
+++ src/Crispin.Tests/Projections/AllTogglesTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using Crispin.Events;
+using Crispin.Projections;
+using Shouldly;
+using Xunit;
+
+namespace Crispin.Tests.Projections
+{
+	public class AllTogglesTests
+	{
+		private readonly AllToggles _projection;
+
+		public AllTogglesTests()
+		{
+			_projection = new AllToggles();
+		}
+
+		[Fact]
+		public void When_no_events_have_been_processed()
+		{
+			_projection.Toggles.ShouldBeEmpty();
+		}
+
+		[Fact]
+		public void When_a_single_toggle_has_been_created()
+		{
+			var created = new ToggleCreated(Guid.NewGuid(), "toggle-1", "");
+			_projection.Consume(created);
+
+			_projection.Toggles.ShouldBe(new Dictionary<Guid, string>
+			{
+				{ created.ID, created.Name }
+			});
+		}
+
+		[Fact]
+		public void When_multiple_toggles_have_been_created()
+		{
+			var first = new ToggleCreated(Guid.NewGuid(), "toggle-1", "");
+			var second = new ToggleCreated(Guid.NewGuid(), "toggle-1", "");
+			var third = new ToggleCreated(Guid.NewGuid(), "toggle-1", "");
+
+			_projection.Consume(first);
+			_projection.Consume(second);
+			_projection.Consume(third);
+
+			_projection.Toggles.ShouldBe(new Dictionary<Guid, string>
+			{
+				{ first.ID, first.Name },
+				{ second.ID, second.Name },
+				{ third.ID, third.Name }
+			});
+		}
+
+		[Fact]
+		public void When_a_toggle_is_switched_on()
+		{
+			var created = new ToggleCreated(Guid.NewGuid(), "toggle-1", "");
+
+			_projection.Consume(created);
+			_projection.Consume(new ToggleSwitchedOn());
+
+			_projection.Toggles.ShouldBe(new Dictionary<Guid, string>
+			{
+				{ created.ID, created.Name }
+			});
+		}
+	}
+}
---
+++ src/Crispin/Projections/AllToggles.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Crispin.Events;
+
+namespace Crispin.Projections
+{
+	public class AllToggles
+	{
+		public IEnumerable<KeyValuePair<Guid, string>> Toggles => _toggles;
+
+		private readonly Dictionary<Guid, string> _toggles;
+		private readonly Dictionary<Type, Action<object>> _handlers;
+
+		public AllToggles()
+		{
+			_toggles = new Dictionary<Guid, string>();
+			_handlers = new Dictionary<Type, Action<object>>();
+
+			Register<ToggleCreated>(Apply);
+		}
+
+		protected void Register<TEvent>(Action<TEvent> handler) => _handlers.Add(typeof(TEvent), e => handler((TEvent)e));
+
+		public void Consume(object @event)
+		{
+			Action<object> apply;
+			if (_handlers.TryGetValue(@event.GetType(), out apply))
+				apply(@event);
+		}
+
+		private void Apply(ToggleCreated e) => _toggles[e.ID] = e.Name;
+	}
+}
