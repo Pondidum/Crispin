f86edf31ee997afca60d8bdc5522035fa298287f
andy <andy@andydote.co.uk>
2017-08-11 22:15:49 +0300

    projections: extract projection base

---
+++ src/Crispin/Projections/Projection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Crispin.Projections
+{
+	public abstract class Projection
+	{
+		private readonly Dictionary<Type, Action<object>> _handlers;
+
+		protected Projection()
+		{
+			_handlers = new Dictionary<Type, Action<object>>();
+		}
+
+		protected void Register<TEvent>(Action<TEvent> handler) => _handlers.Add(typeof(TEvent), e => handler((TEvent)e));
+
+		public void Consume(object @event)
+		{
+			Action<object> apply;
+			if (_handlers.TryGetValue(@event.GetType(), out apply))
+				apply(@event);
+		}
+	}
+}
