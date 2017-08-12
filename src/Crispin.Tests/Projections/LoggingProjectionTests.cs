59f59f5b36fdd2055b0304e4a50e67447e7f5e21
andy <andy@andydote.co.uk>
2017-08-12 19:38:57 +0300

    projections: add logging projection

---
+++ src/Crispin.Tests/Projections/LoggingProjectionTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using Crispin.Infrastructure;
+using Crispin.Projections;
+using Shouldly;
+using Xunit;
+
+namespace Crispin.Tests.Projections
+{
+	public class LoggingProjectionTests
+	{
+		private readonly LoggingProjection _projection;
+
+		public LoggingProjectionTests()
+		{
+			_projection = new LoggingProjection();
+		}
+
+		[Fact]
+		public void When_consuming_events()
+		{
+			var now = DateTime.Now;
+			var first = new First { TimeStamp = now.AddSeconds(1) };
+			var second = new Second { TimeStamp = now.AddSeconds(2) };
+
+			_projection.Consume(first);
+			_projection.Consume(second);
+
+			_projection.Messages.Select(e => e.Message).ShouldBe(new[]
+			{
+				first.ToString(),
+				second.ToString()
+			});
+
+			_projection.Messages.Select(e => e.TimeStamp).ShouldBe(new[]
+			{
+				now.AddSeconds(1),
+				now.AddSeconds(2)
+			});
+		}
+
+		public class First : Event {}
+		public class Second : Event {}
+	}
+}
---
+++ src/Crispin/Projections/LoggingProjection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Crispin.Infrastructure;
+
+namespace Crispin.Projections
+{
+	public class LoggingProjection : Projection
+	{
+		public List<LogEntry> Messages { get; }
+
+		public LoggingProjection()
+		{
+			Messages = new List<LogEntry>();
+			RegisterAll(@event => Messages.Add(new LogEntry
+			{
+				TimeStamp = @event.TimeStamp,
+				Message = @event.ToString()
+			}));
+		}
+
+		public class LogEntry
+		{
+			public DateTime TimeStamp { get; set; }
+			public string Message { get; set; }
+		}
+	}
+}
