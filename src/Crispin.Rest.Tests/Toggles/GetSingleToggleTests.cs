e3e810216f9faf7fb183e088cead27aaaa9c80c3
andy <andy@andydote.co.uk>
2017-08-26 19:22:10 +0300

    toggleControllerTests: test fetching a toggle by id and name

--- src/Crispin.Rest.Tests/Toggles/GetSingleToggleTests.cs
+++ src/Crispin.Rest.Tests/Toggles/GetSingleToggleTests.cs
@@ -11,17 +11,35 @@ namespace Crispin.Rest.Tests.Toggles
 {
 	public class GetSingleToggleTests : TogglesControllerTests
 	{
+		public GetSingleToggleTests()
+		{
+			Mediator
+				.Send(Arg.Any<GetToggleRequest>())
+				.Returns(new GetToggleResponse { Toggle = new ToggleView() });
+
+			Mediator
+				.Send(Arg.Any<GetToggleByNameRequest>())
+				.Returns(new GetToggleResponse { Toggle = new ToggleView() });
+		}
+
 		[Fact]
 		public async Task When_fetching_a_single_toggle_by_id()
 		{
-			var view = new ToggleView();
-			Mediator
-				.Send(Arg.Any<GetToggleRequest>())
-				.Returns(new GetToggleResponse { Toggle = view });
+			var toggleId = Guid.NewGuid();
+			var response = (JsonResult)await Controller.Get(toggleId);
 
-			var response = (JsonResult)await Controller.Get(Guid.NewGuid());
+			await Mediator.Received().Send(Arg.Is<GetToggleRequest>(req => req.ToggleID == toggleId));
+			response.Value.ShouldBeOfType<ToggleView>();
+		}
 
-			response.Value.ShouldBe(view);
+		[Fact]
+		public async Task When_fetching_a_single_toggle_by_name()
+		{
+			var toggleName = "toggle-name";
+			var response = (JsonResult)await Controller.Get(toggleName);
+
+			await Mediator.Received().Send(Arg.Is<GetToggleByNameRequest>(req => req.Name == toggleName));
+			response.Value.ShouldBeOfType<ToggleView>();
 		}
 	}
 }
