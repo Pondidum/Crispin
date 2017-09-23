710d2567408e8805dcd4e3dafb5e922082bec903
andy <andy@andydote.co.uk>
2017-09-23 18:18:15 +0300

    tests: ensure all request objects dont locate by toggleId

---
+++ src/Crispin.Tests/Handlers/RequestRulesTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MediatR;
+using Shouldly;
+using Xunit;
+
+namespace Crispin.Tests.Handlers
+{
+	public class RequestRulesTests
+	{
+		[Fact]
+		public void There_should_be_requests() => AllRequests.ShouldNotBeEmpty();
+
+		[Theory]
+		[MemberData(nameof(AllRequests))]
+		public void All_requests_must_not_have_a_toggle_id(Type request) => request
+			.GetProperties()
+			.Where(prop => prop.PropertyType == typeof(ToggleID)).ShouldBeEmpty();
+
+		public static IEnumerable<object[]> AllRequests => typeof(ToggleLocator)
+			.Assembly
+			.GetExportedTypes()
+			.Where(t => t.IsClass && t.IsAbstract == false)
+			.Where(type => Implements(type, typeof(IRequest<>)))
+			.Select(type => new object[] { type });
+
+		private static bool Implements(Type type, Type lookFor) => type
+			.GetInterfaces()
+			.Where(i => i.IsGenericType)
+			.Select(i => i.GetGenericTypeDefinition())
+			.Contains(lookFor);
+	}
+}
