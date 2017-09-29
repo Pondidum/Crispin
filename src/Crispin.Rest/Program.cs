7c48967e8d721872c38f6c3f315a593417a46230
andy <andy@andydote.co.uk>
2017-09-29 20:57:45 +0300

    rest: add appsettings loading, with logger level config

--- src/Crispin.Rest/Program.cs
+++ src/Crispin.Rest/Program.cs
@@ -20,6 +20,18 @@ namespace Crispin.Rest
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .ConfigureAppConfiguration((hosting, config) =>
+                {
+                    var env = hosting.HostingEnvironment;
+                    config
+                        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                })
+                .ConfigureLogging((hosting, logging) =>
+                {
+                    logging.AddConfiguration(hosting.Configuration.GetSection("Logging"));
+                    logging.AddDebug();
+                })
                 .Build();
     }
 }
