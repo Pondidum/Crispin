069dde99be7201bfd8b3c2d92dd929a3c724a01c
andy <andy@andydote.co.uk>
2017-10-01 17:09:16 +0300

    statistics: implement composite statistics writer

---
+++ src/Crispin.Tests/Infrastructure/Statistics/CompositeStatisticsWriterTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using Crispin.Infrastructure.Statistics;
+using NSubstitute;
+using Xunit;
+
+namespace Crispin.Tests.Infrastructure.Statistics
+{
+	public class CompositeStatisticsWriterTests
+	{
+		private readonly IStatisticsWriter _first;
+		private readonly IStatisticsWriter _second;
+		private readonly CompositeStatisticsWriter _writer;
+
+		public CompositeStatisticsWriterTests()
+		{
+			_first = Substitute.For<IStatisticsWriter>();
+			_second = Substitute.For<IStatisticsWriter>();
+
+			_writer = new CompositeStatisticsWriter(_first, _second);
+		}
+
+		[Fact]
+		public async Task When_write_is_called()
+		{
+			const string key = "some key";
+			const string value = "some value";
+
+			await _writer.Write(key, value);
+
+			await _first.Received().Write(key, value);
+			await _second.Received().Write(key, value);
+		}
+
+		[Fact]
+		public async Task When_writeCount_is_called()
+		{
+			const string key = "some key";
+
+			await _writer.WriteCount(key);
+
+			await _first.Received().WriteCount(key);
+			await _second.Received().WriteCount(key);
+		}
+	}
+}
---
+++ src/Crispin/Infrastructure/Statistics/CompositeStatisticsWriter.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crispin.Infrastructure.Statistics
+{
+	public class CompositeStatisticsWriter : IStatisticsWriter
+	{
+		private readonly IStatisticsWriter[] _writers;
+
+		public CompositeStatisticsWriter(params IStatisticsWriter[] writers)
+		{
+			_writers = writers.ToArray();
+		}
+
+		public async Task Write(string key, string value)
+		{
+			await Task.WhenAll(_writers
+				.Select(writer => writer.Write(key, value))
+				.ToArray());
+		}
+
+		public async Task WriteCount(string key)
+		{
+			await Task.WhenAll(_writers
+				.Select(writer => writer.WriteCount(key))
+				.ToArray());
+		}
+	}
+}
