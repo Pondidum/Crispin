9d8f7eb81b67317b056848998d70b8e07f00b39d
andy <andy@andydote.co.uk>
2017-10-03 22:21:29 +0300

    statistics: implement template property parsing

--- src/Crispin.Tests/Infrastructure/Statistics/ExtensionsTests.cs
+++ src/Crispin.Tests/Infrastructure/Statistics/ExtensionsTests.cs
@@ -8,21 +8,13 @@ namespace Crispin.Tests.Infrastructure.Statistics
 {
 	public class ExtensionsTests
 	{
-		public static IEnumerable<object[]> StringRenderingCases
+		public static IEnumerable<object[]> StringRenderingCases => new List<object[]>
 		{
-			get
-			{
-				var values = new List<object[]>
-				{
-					new object[] { "with.no.replacements", Array.Empty<object>(), "with.no.replacements" },
-					new object[] { "with.{one}.replacement", new[] { "test" }, "with.test.replacement" },
-					new object[] { "with.{first}.{second}.replacement", new[] { "one", "two" }, "with.one.two.replacement" },
-					new object[] { "with.{first}.{second}.replacement", new[] { "one", "two", "three" }, "with.one.two.replacement" }
-				};
-
-				return values;
-			}
-		}
+			new object[] { "with.no.replacements", Array.Empty<object>(), "with.no.replacements" },
+			new object[] { "with.{one}.replacement", new[] { "test" }, "with.test.replacement" },
+			new object[] { "with.{first}.{second}.replacement", new[] { "one", "two" }, "with.one.two.replacement" },
+			new object[] { "with.{first}.{second}.replacement", new[] { "one", "two", "three" }, "with.one.two.replacement" }
+		};
 
 		[Theory]
 		[MemberData(nameof(StringRenderingCases))]
@@ -36,7 +28,36 @@ namespace Crispin.Tests.Infrastructure.Statistics
 		[Fact]
 		public void When_there_are_too_few_parameters()
 		{
-			Should.Throw<FormatException>(() =>"some.{test}{value}{replacement}".Render("one"));
+			Should.Throw<FormatException>(() => "some.{test}{value}{replacement}".Render("one"));
+		}
+
+		public static IEnumerable<object[]> TemplateCases => new List<object[]>
+		{
+			new object[]
+			{
+				"with.no.replacements",
+				Array.Empty<object>(),
+				new Dictionary<string, object>()
+			},
+			new object[]
+			{
+				"with.{one}.replacement",
+				new[] { "test" },
+				new Dictionary<string, object> { { "one", "test" } }
+			},
+			new object[]
+			{
+				"with.{first}.{second}.replacement",
+				new[] { "one", "two" },
+				new Dictionary<string, object> { { "first", "one" }, { "second", "two" } }
+			}
+		};
+
+		[Theory]
+		[MemberData(nameof(TemplateCases))]
+		public void When_building_property_map(string format, object[] parameters, Dictionary<string, object> expected)
+		{
+			format.BuildPropertyMap(parameters).ShouldBe(expected);
 		}
 	}
 }
--- src/Crispin/Infrastructure/Statistics/Extensions.cs
+++ src/Crispin/Infrastructure/Statistics/Extensions.cs
@@ -1,16 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text.RegularExpressions;
 
 namespace Crispin.Infrastructure.Statistics
 {
 	public static class Extensions
 	{
+		private static readonly Regex Groups = new Regex(@"\{(.*?)\}");
+
 		public static string Render(this string format, params object[] parameters)
 		{
-			var rx = new Regex(@"\{(.*?)\}");
 			var index = 0;
 
-			return rx.Replace(
+			return Groups.Replace(
 				format,
 				match =>
 				{
@@ -19,6 +23,17 @@ namespace Crispin.Infrastructure.Statistics
 
 					return parameters[index++].ToString();
 				});
+		}
+
+		public static IDictionary<string, object> BuildPropertyMap(this string template, params object[] parameters)
+		{
+			var index = 0;
+			return Groups
+				.Matches(template)
+				.Cast<Match>()
+				.ToDictionary(
+					match => match.Groups[1].Value,
+					match => parameters[index++]);
 		}
 	}
 }
