e81863e20a7bfa1ee1b888f8688092da1ea5339c
andy <andy@andydote.co.uk>
2017-10-03 21:35:53 +0300

    updateToggleStateStatistics: use template and values

--- src/Crispin/Handlers/UpdateState/UpdateToggleStateStatistics.cs
+++ src/Crispin/Handlers/UpdateState/UpdateToggleStateStatistics.cs
@@ -17,17 +17,28 @@ namespace Crispin.Handlers.UpdateState
 
 			if (request.Anonymous.HasValue)
 			{
-				await writer.WriteCount($"toggle.{response.ToggleID}.state.anonymous.{state(request.Anonymous)}");
+				await writer.WriteCount(
+					"toggle.{toggleID}.state.anonymous.{state}",
+					response.ToggleID,
+					state(request.Anonymous));
 			}
 
 			foreach (var user in request.Users)
 			{
-				await writer.WriteCount($"toggle.{response.ToggleID}.state.users.{user.Key}.{state(user.Value)}");
+				await writer.WriteCount(
+					"toggle.{toggleID}.state.users.{userID}.{state}",
+					response.ToggleID,
+					user.Key,
+					state(user.Value));
 			}
 
 			foreach (var group in request.Groups)
 			{
-				await writer.WriteCount($"toggle.{response.ToggleID}.state.groups.{group.Key}.{state(group.Value)}");
+				await writer.WriteCount(
+					"toggle.{toggleID}.state.groups.{groupID}.{state}",
+					response.ToggleID,
+					group.Key,
+					state(group.Value));
 			}
 		}
 	}
