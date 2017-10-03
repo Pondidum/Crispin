c72170be388b586f5ef9088b2209305d8674195d
andy <andy@andydote.co.uk>
2017-10-03 21:32:05 +0300

    statistics: implement named parameter rendering extension

---
+++ src/Crispin.Tests/Infrastructure/Statistics/ExtensionsTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Crispin.Infrastructure.Statistics;
+using Shouldly;
+using Xunit;
+
+namespace Crispin.Tests.Infrastructure.Statistics
+{
+	public class ExtensionsTests
+	{
+		public static IEnumerable<object[]> StringRenderingCases
+		{
+			get
+			{
+				var values = new List<object[]>
+				{
+					new object[] { "with.no.replacements", Array.Empty<object>(), "with.no.replacements" },
+					new object[] { "with.{one}.replacement", new[] { "test" }, "with.test.replacement" },
+					new object[] { "with.{first}.{second}.replacement", new[] { "one", "two" }, "with.one.two.replacement" },
+					new object[] { "with.{first}.{second}.replacement", new[] { "one", "two", "three" }, "with.one.two.replacement" }
+				};
+
+				return values;
+			}
+		}
+
+		[Theory]
+		[MemberData(nameof(StringRenderingCases))]
+		public void When_rendering_a_valid_string_and_parameters(string format, object[] parameters, string expected)
+		{
+			format
+				.Render(parameters)
+				.ShouldBe(expected);
+		}
+
+		[Fact]
+		public void When_there_are_too_few_parameters()
+		{
+			Should.Throw<FormatException>(() =>"some.{test}{value}{replacement}".Render("one"));
+		}
+	}
+}
--- src/Crispin/Infrastructure/Statistics/Extensions.cs
+++ src/Crispin/Infrastructure/Statistics/Extensions.cs
@@ -1,10 +1,24 @@
-﻿namespace Crispin.Infrastructure.Statistics
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Crispin.Infrastructure.Statistics
 {
 	public static class Extensions
 	{
 		public static string Render(this string format, params object[] parameters)
 		{
-			return format;
+			var rx = new Regex(@"\{(.*?)\}");
+			var index = 0;
+
+			return rx.Replace(
+				format,
+				match =>
+				{
+					if (index >= parameters.Length)
+						throw new FormatException("Not enough parameters were provided to render the string");
+
+					return parameters[index++].ToString();
+				});
 		}
 	}
 }
