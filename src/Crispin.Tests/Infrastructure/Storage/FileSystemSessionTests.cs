ce540ccbca0f9d1d8ea5cbb2d4681ad5e0a66b2f
andy <andy@andydote.co.uk>
2017-10-07 18:57:19 +0300

    storage: start on FileSystemSession

---
+++ src/Crispin.Tests/Infrastructure/Storage/FileSystemSessionTests.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Crispin.Events;
+using Crispin.Infrastructure;
+using Crispin.Infrastructure.Storage;
+using FileSystem;
+using Newtonsoft.Json;
+using NSubstitute;
+using Shouldly;
+using Xunit;
+
+namespace Crispin.Tests.Infrastructure.Storage
+{
+	public class FileSystemSessionTests
+	{
+		private const string Root = "./store";
+
+		private readonly InMemoryFileSystem _fs;
+		private readonly FileSystemSession _session;
+		private readonly Dictionary<Type, Func<List<Event>, AggregateRoot>> _builders;
+		private readonly ToggleID _aggregateID;
+		private readonly EditorID _editor;
+		private readonly IGroupMembership _membership;
+
+		public FileSystemSessionTests()
+		{
+			_builders = new Dictionary<Type, Func<List<Event>, AggregateRoot>>
+			{
+				{ typeof(Toggle), Toggle.LoadFrom }
+			};
+			
+			_fs = new InMemoryFileSystem();
+			_fs.CreateDirectory(Root).Wait();
+
+			_session = new FileSystemSession(_fs, _builders, Root);
+			_membership = Substitute.For<IGroupMembership>();
+			_editor = EditorID.Parse("wat");
+			_aggregateID = ToggleID.CreateNew();
+		}
+
+		
+		[Fact]
+		public void When_there_is_no_builder_for_an_aggregate()
+		{
+			_builders.Clear();
+
+			Should.Throw<NotSupportedException>(() => _session.LoadAggregate<Toggle>(_aggregateID));
+		}
+
+		[Fact]
+		public void When_there_is_no_aggregate_stored()
+		{
+			Should.Throw<KeyNotFoundException>(() => _session.LoadAggregate<Toggle>(_aggregateID));
+		}
+		
+		[Fact]
+		public async Task When_there_are_no_events_for_an_aggregate_stored()
+		{
+			await _fs.WriteFile(
+				Path.Combine(Root, _aggregateID.ToString()),
+				stream => Task.CompletedTask);
+
+			Should.Throw<KeyNotFoundException>(() => _session.LoadAggregate<Toggle>(_aggregateID));
+		}
+
+		private async Task WriteEvents(params object[] events)
+		{
+			var jsonSettings = new JsonSerializerSettings
+			{
+				Formatting = Formatting.None,
+				TypeNameHandling = TypeNameHandling.Objects
+			};
+
+			await _fs.WriteFile(Path.Combine(Root, _aggregateID.ToString()), stream =>
+			{
+				using (var writer = new StreamWriter(stream))
+					events
+						.Select(e => JsonConvert.SerializeObject(e, jsonSettings))
+						.Each(line => writer.WriteLine(line));
+				
+				return Task.CompletedTask;
+			});
+		}
+		
+		[Fact]
+		public async Task When_an_aggregate_is_loaded()
+		{
+			await WriteEvents(
+				new ToggleCreated(_editor, _aggregateID, "First", "hi"),
+				new TagAdded(_editor, "one"),
+				new ToggleSwitchedOnForUser(_editor, UserID.Parse("user-1"))
+			);
+
+			var toggle = _session.LoadAggregate<Toggle>(_aggregateID);
+
+			toggle.ShouldSatisfyAllConditions(
+				() => toggle.ID.ShouldBe(_aggregateID),
+				() => toggle.IsActive(_membership, UserID.Parse("user-1")).ShouldBeTrue(),
+				() => toggle.Tags.ShouldContain("one")
+			);
+		}
+	}
+}
---
+++ src/Crispin/Infrastructure/Storage/FileSystemStorage.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Crispin.Events;
+using FileSystem;
+using Newtonsoft.Json;
+
+namespace Crispin.Infrastructure.Storage
+{
+	public class FileSystemSession : IStorageSession
+	{
+		private readonly IFileSystem _fileSystem;
+		private readonly Dictionary<Type, Func<List<Event>, AggregateRoot>> _builders;
+		private readonly string _root;
+
+		public FileSystemSession(IFileSystem fileSystem, Dictionary<Type, Func<List<Event>, AggregateRoot>> builders, string root)
+		{
+			_fileSystem = fileSystem;
+			_builders = builders;
+			_root = root;
+		}
+
+		public void Dispose()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Open()
+		{
+			throw new NotImplementedException();
+		}
+
+		public TProjection LoadProjection<TProjection>() where TProjection : Projection
+		{
+			throw new NotImplementedException();
+		}
+
+		public TAggregate LoadAggregate<TAggregate>(ToggleID aggregateID) where TAggregate : AggregateRoot
+		{
+			try
+			{
+				return LoadAggregateInternal<TAggregate>(aggregateID).Result;
+			}
+			catch (AggregateException e)
+			{
+				if (e.InnerException != null)
+					throw e.InnerException;
+				else
+					throw;
+			}
+		}
+
+		private async Task<TAggregate> LoadAggregateInternal<TAggregate>(ToggleID aggregateID) where TAggregate : AggregateRoot
+		{
+			var jsonSettings = new JsonSerializerSettings
+			{
+				Formatting = Formatting.None,
+				TypeNameHandling = TypeNameHandling.Objects
+			};
+
+			Func<List<Event>, AggregateRoot> builder;
+
+			if (_builders.TryGetValue(typeof(TAggregate), out builder) == false)
+				throw new NotSupportedException($"No builder for type {typeof(TAggregate).Name} found.");
+
+			var aggregatePath = Path.Combine(_root, aggregateID.ToString());
+
+			if (await _fileSystem.FileExists(aggregatePath) == false)
+				throw new KeyNotFoundException($"Unable to find an aggregate with ID {aggregateID}");
+
+			var events = (await _fileSystem.ReadFileLines(aggregatePath))
+				.Select(e => JsonConvert.DeserializeObject(e, jsonSettings))
+				.Cast<Event>()
+				.ToList();
+
+			if (events.Any() == false)
+				throw new KeyNotFoundException($"Unable to find an aggregate with ID {aggregateID}");
+
+			var aggregate = builder(events.ToList());
+
+			return (TAggregate)aggregate;
+		}
+
+		public void Save<TAggregate>(TAggregate aggregate) where TAggregate : AggregateRoot, IEvented
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Commit()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
