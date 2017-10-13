4c15795d62bfbfbd120348ce2bab211d8ec63152
andy <andy@andydote.co.uk>
2017-10-13 20:54:15 +0300

    tests: refactor inMemoryStorageTests to be abstract base class

--- src/Crispin.Tests/Infrastructure/Storage/InMemoryStorageTests.cs
+++ src/Crispin.Tests/Infrastructure/Storage/InMemoryStorageTests.cs
@@ -1,53 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Crispin.Infrastructure.Storage;
-using Crispin.Projections;
-using NSubstitute;
-using Shouldly;
-using Xunit;
+﻿using Crispin.Infrastructure.Storage;
 
 namespace Crispin.Tests.Infrastructure.Storage
 {
-	public class InMemoryStorageTests
+	public class InMemoryStorageTests : StorageTests
 	{
-		private readonly InMemoryStorage _storage;
-
 		public InMemoryStorageTests()
 		{
-			_storage = new InMemoryStorage();
-		}
-
-		[Fact]
-		public void When_a_projection_is_registered()
-		{
-			var projection = new AllToggles();
-			_storage.RegisterProjection(projection);
-
-			using (var session = _storage.BeginSession())
-				session.Save(Toggle.CreateNew(EditorID.Parse("test"), "Test", "no"));
-
-			projection.Toggles.Count().ShouldBe(1);
-		}
-
-		[Fact]
-		public void When_there_isnt_an_aggregates_registered()
-		{
-			using (var session = _storage.BeginSession())
-			{
-				Should.Throw<NotSupportedException>(() => session.LoadAggregate<Toggle>(ToggleID.CreateNew()));
-			}
-		}
-
-		[Fact]
-		public void When_an_aggregate_is_registered()
-		{
-			_storage.RegisterBuilder(Toggle.LoadFrom);
-
-			using (var session = _storage.BeginSession())
-			{
-				Should.Throw<KeyNotFoundException>(() => session.LoadAggregate<Toggle>(ToggleID.CreateNew()));
-			}
+			Storage = new InMemoryStorage();
 		}
 	}
 }
---
+++ src/Crispin.Tests/Infrastructure/Storage/StorageTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Crispin.Infrastructure.Storage;
+using Crispin.Projections;
+using Shouldly;
+using Xunit;
+
+namespace Crispin.Tests.Infrastructure.Storage
+{
+	public abstract class StorageTests
+	{
+		protected IStorage Storage { get; set; }
+
+		[Fact]
+		public void When_a_projection_is_registered()
+		{
+			var projection = new AllToggles();
+			Storage.RegisterProjection(projection);
+
+			using (var session = Storage.BeginSession())
+				session.Save(Toggle.CreateNew(EditorID.Parse("test"), "Test", "no"));
+
+			projection.Toggles.Count().ShouldBe(1);
+		}
+
+		[Fact]
+		public void When_there_isnt_an_aggregates_registered()
+		{
+			using (var session = Storage.BeginSession())
+			{
+				Should.Throw<NotSupportedException>(() => session.LoadAggregate<Toggle>(ToggleID.CreateNew()));
+			}
+		}
+
+		[Fact]
+		public void When_an_aggregate_is_registered()
+		{
+			Storage.RegisterBuilder(Toggle.LoadFrom);
+
+			using (var session = Storage.BeginSession())
+			{
+				Should.Throw<KeyNotFoundException>(() => session.LoadAggregate<Toggle>(ToggleID.CreateNew()));
+			}
+		}
+	}
+}
