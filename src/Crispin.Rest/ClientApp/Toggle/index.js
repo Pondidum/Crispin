3c4ba0260fa5540be973869f23c07c50c4fe82b4
andy <andy@andydote.co.uk>
2017-10-21 19:47:48 +0300

    webui: add states to toggle view

--- src/Crispin.Rest/ClientApp/Toggle/index.js
+++ src/Crispin.Rest/ClientApp/Toggle/index.js
@@ -6,12 +6,20 @@ import React from 'react'
 // User1: Off
 // Group2: Off
 
+const toggleState = (state) => state === 1 ? 'On' : 'Off';
+
 const Toggle = ({ toggle }) => {
-    const { name, description } = toggle;
+    const { name, description, state } = toggle;
+
+    const specificStates =
+        Object.keys(state.users).length +
+        Object.keys(state.groups).length;
+
     return (
         <div className="toggle panel panel-default">
             <h3>{name}</h3>
             <span>{description}</span>
+            <p>Default: {toggleState(state.anonymous)}{specificStates > 0 ? ` and ${specificStates} other states.`: '.'}</p>
         </div>
     )
 }
