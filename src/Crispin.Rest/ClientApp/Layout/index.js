28db6377f84a0a0cd2c752361b431027fcc536bf
andy <andy@andydote.co.uk>
2017-10-31 20:44:58 +0200

    ui: add home link to sidebar

--- src/Crispin.Rest/ClientApp/Layout/index.js
+++ src/Crispin.Rest/ClientApp/Layout/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import { Col, Row } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom';
 
 export default ({ children }) => (
   <div className="container-fluid" id="root">
     <Row>
       <Col sm={3} md={2} className="sidebar">
-        <h1>Sidebar</h1>
+        <h1>Crispin</h1>
+        <ul className="list-unstyled">
+          <li>
+            <NavLink exact to={ '/' } activeClassName='active'>
+              <span className='glyphicon glyphicon-home'></span> Home
+            </NavLink>
+          </li>
+        </ul>
       </Col>
       <Col sm={9} md={10} smOffset={3} mdOffset={2} className="main">
         { children }
