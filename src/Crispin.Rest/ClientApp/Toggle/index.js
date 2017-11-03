b000080393eda92171cc4273298ec2ceb06c3ed5
andy <andy@andydote.co.uk>
2017-11-03 18:44:10 +0200

    webui: fix toggle indentation

--- src/Crispin.Rest/ClientApp/Toggle/index.js
+++ src/Crispin.Rest/ClientApp/Toggle/index.js
@@ -9,19 +9,19 @@ import React from 'react'
 const toggleState = (state) => state === 1 ? 'On' : 'Off';
 
 const Toggle = ({ toggle }) => {
-    const { name, description, state } = toggle;
+  const { name, description, state } = toggle;
 
-    const specificStates =
-        Object.keys(state.users).length +
-        Object.keys(state.groups).length;
+  const specificStates =
+    Object.keys(state.users).length +
+    Object.keys(state.groups).length;
 
-    return (
-        <div className="toggle panel panel-default col-sm-12">
-            <h3>{name}</h3>
-            <span>{description}</span>
-            <p>Default: {toggleState(state.anonymous)}{specificStates > 0 ? ` and ${specificStates} other states.`: '.'}</p>
-        </div>
-    )
+  return (
+    <div className="toggle panel panel-default col-sm-12">
+      <h3>{name}</h3>
+      <span>{description}</span>
+      <p>Default: {toggleState(state.anonymous)}{specificStates > 0 ? ` and ${specificStates} other states.`: '.'}</p>
+    </div>
+  )
 }
 
 export default Toggle
