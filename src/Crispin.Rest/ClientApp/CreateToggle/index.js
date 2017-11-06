5392e365958bea2049b1884eb2d9b18ac60032c4
andy <andy@andydote.co.uk>
2017-11-06 21:26:29 +0200

    webui: add form for creating toggles

--- src/Crispin.Rest/ClientApp/CreateToggle/index.js
+++ src/Crispin.Rest/ClientApp/CreateToggle/index.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 import { Button, Modal } from "react-bootstrap";
+import { FormGroup, ControlLabel, FormControl } from "react-bootstrap";
 
 class CreateToggle extends Component {
   constructor() {
     super();
-    this.state = { showModal: false };
+    this.state = {
+      showModal: false,
+      name: "",
+      description: ""
+    };
   }
 
   render() {
@@ -25,7 +30,28 @@ class CreateToggle extends Component {
           <Modal.Header closeButton>
             <Modal.Title>Create new Toggle</Modal.Title>
           </Modal.Header>
-          <Modal.Body>Some form here</Modal.Body>
+          <Modal.Body>
+            <form>
+              <FormGroup controlId="toggleName">
+                <ControlLabel>Name</ControlLabel>
+                <FormControl
+                  type="text"
+                  placeholder="My-Toggle"
+                  value={this.state.name}
+                  onChange={e => this.setState({ name: e.target.value })}
+                />
+              </FormGroup>
+              <FormGroup controlId="toggleDescription">
+                <ControlLabel>Description</ControlLabel>
+                <FormControl
+                  type="text"
+                  placeholder="some short description of the toggle"
+                  value={this.state.description}
+                  onChange={e => this.setState({ description: e.target.value })}
+                />
+              </FormGroup>
+            </form>
+          </Modal.Body>
           <Modal.Footer>
             <Button onClick={close} bsStyle="primary">
               Create
