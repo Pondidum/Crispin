95d2fba2f569add067ea3b24ebae09a86180b46d
andy <andy@andydote.co.uk>
2017-11-06 20:47:48 +0200

    webui: add trend graph to toggle with hardcoded data

--- src/Crispin.Rest/ClientApp/Toggle/index.js
+++ src/Crispin.Rest/ClientApp/Toggle/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
+import Trend from "react-trend";
 import ToggleState from "./toggleState";
 
 const Toggle = ({ toggle }) => {
@@ -6,7 +8,20 @@ const Toggle = ({ toggle }) => {
 
   return (
     <div className="toggle panel panel-default col-sm-12">
-      <h3>{name}</h3>
+      <Row>
+        <Col sm={6}>
+          <h3>{name}</h3>
+        </Col>
+        <Col sm={6}>
+          <Trend
+            smooth
+            height={56}
+            strokeWidth={2}
+            gradient={["purple", "violet"]}
+            data={[0, 10, 5, 22, 3.6, 11]}
+          />
+        </Col>
+      </Row>
       <span>{description}</span>
       <ToggleState state={state} />
     </div>
