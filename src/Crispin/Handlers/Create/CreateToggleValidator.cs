9ab8d7aec26a1c90233b22690a666059cfc9f444
andy <andy@andydote.co.uk>
2017-11-12 16:18:43 +0200

    createToggleValidator: check name is not already used

--- src/Crispin/Handlers/Create/CreateToggleValidator.cs
+++ src/Crispin/Handlers/Create/CreateToggleValidator.cs
@@ -1,15 +1,35 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using Crispin.Infrastructure.Storage;
 using Crispin.Infrastructure.Validation;
+using Crispin.Projections;
 
 namespace Crispin.Handlers.Create
 {
 	public class CreateToggleValidator : IRequestValidator<CreateToggleRequest>
 	{
+		private readonly IStorage _storage;
+
+		public CreateToggleValidator(IStorage storage)
+		{
+			_storage = storage;
+		}
+
 		public ICollection<string> Validate(CreateToggleRequest request)
 		{
 			if (string.IsNullOrWhiteSpace(request.Name))
 				return new[] { "The Name property must be filled out" };
+
+			using (var session = _storage.BeginSession().Result)
+			{
+				var view = session.LoadProjection<AllToggles>().Result.Toggles;
+
+				if (view.Any(tv => string.Equals(tv.Name, request.Name, StringComparison.OrdinalIgnoreCase)))
+				{
+					return new[] { $"The Name '{request.Name}' is already in use" };
+				}
+			}
 
 			return Array.Empty<string>();
 		}
