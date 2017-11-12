c032fcd6a1d210fb1c28030a11c69e2ea146d06c
andy <andy@andydote.co.uk>
2017-11-12 16:15:11 +0200

    tests: implement create toggle pipeline tests

---
+++ src/Crispin.Tests/Handlers/HandlerPipelineTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using Crispin.Infrastructure;
+using Crispin.Infrastructure.Statistics;
+using Crispin.Infrastructure.Storage;
+using Crispin.Projections;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using StructureMap;
+
+namespace Crispin.Tests.Handlers
+{
+	public abstract class HandlerPipelineTest<TRequest, TResponse>
+		where TRequest : IRequest<TResponse>
+	{
+		protected TResponse Response { get; set; }
+		protected InMemoryStorage Storage { get; }
+		protected EditorID Editor { get; }
+
+		private readonly IMediator _mediator;
+
+		protected HandlerPipelineTest()
+		{
+			Editor = EditorID.Parse("me");
+			Storage = new InMemoryStorage();
+			Storage.RegisterBuilder(Toggle.LoadFrom);
+			Storage.RegisterProjection(new AllToggles());
+
+			var container = new Container(_ =>
+			{
+				_.AddRegistry<MediatrRegistry>();
+
+				_.For<IStorage>().Use(Storage);
+				_.For<ILoggerFactory>().Use(Substitute.For<ILoggerFactory>());
+				_.For(typeof(ILogger<>)).Use(typeof(Logger<>));
+				_.For<IStatisticsWriter>().Use(Substitute.For<IStatisticsWriter>());
+			});
+
+			_mediator = container.GetInstance<IMediator>();
+		}
+
+		protected async Task Send(TRequest message) => Response = await _mediator.Send(message);
+	}
+}
