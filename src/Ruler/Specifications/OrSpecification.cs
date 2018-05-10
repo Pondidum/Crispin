f329d0048971865ac50f9bf9b2da7a89794e753d
andy <andydavies2020@gmail.com>
2018-05-10 20:26:37 +0300

    rules: orSpecification is a constrained anySpecification

--- src/Ruler/Specifications/OrSpecification.cs
+++ src/Ruler/Specifications/OrSpecification.cs
@@ -1,16 +1,10 @@
 ﻿namespace Ruler.Specifications
 {
-	public class OrSpecification<T> : ISpecification<T>
+	public class OrSpecification<T> : AnySpecification<T>
 	{
-		private readonly ISpecification<T> _left;
-		private readonly ISpecification<T> _right;
-
 		public OrSpecification(ISpecification<T> left, ISpecification<T> right)
+			: base(left, right)
 		{
-			_left = left;
-			_right = right;
 		}
-
-		public bool IsMatch(T input) => _left.IsMatch(input) || _right.IsMatch(input);
 	}
 }
