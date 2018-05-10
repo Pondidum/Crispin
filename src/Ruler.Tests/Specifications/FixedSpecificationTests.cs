cf15fb69cdb887f600f555d84bf99ffc513e90bd
andy <andydavies2020@gmail.com>
2018-05-10 23:08:48 +0300

    rules: add fixedSpecification (mostly for testing)

---
+++ src/Ruler.Tests/Specifications/FixedSpecificationTests.cs
@@ -0,0 +1,19 @@
+﻿using Ruler.Specifications;
+using Shouldly;
+using Xunit;
+
+namespace Ruler.Tests.Specifications
+{
+	public class FixedSpecificationTests
+	{
+		[Theory]
+		[InlineData(true)]
+		[InlineData(false)]
+		public void The_specification_returns_the_expected_result(bool value)
+		{
+			var spec = new FixedSpecification<string>(value);
+
+			spec.IsMatch("wat").ShouldBe(value);
+		}
+	}
+}
