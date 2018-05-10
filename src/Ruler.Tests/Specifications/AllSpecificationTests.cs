671f6db282af4b0c52dcf76f35de305c3186d550
andy <andydavies2020@gmail.com>
2018-05-10 20:31:02 +0300

    rules: implement allSpecification

---
+++ src/Ruler.Tests/Specifications/AllSpecificationTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NSubstitute;
+using Ruler.Specifications;
+using Shouldly;
+using Xunit;
+
+namespace Ruler.Tests.Specifications
+{
+	public class AllSpecificationTests
+	{
+		private readonly ISpecification<string>[] _inners;
+		private readonly AllSpecification<string> _sut;
+
+		public AllSpecificationTests()
+		{
+			_inners = Enumerable
+				.Range(0, 5)
+				.Select(i => Substitute.For<ISpecification<string>>())
+				.ToArray();
+			_sut = new AllSpecification<string>(_inners);
+		}
+
+		public static IEnumerable<object[]> Indexes => Enumerable.Range(0, 5).Select(i => new object[] { i });
+
+		[Theory]
+		[MemberData(nameof(Indexes))]
+		public void When_any_are_false(int falseIndex)
+		{
+			for (int i = 0; i < _inners.Length; i++)
+				_inners[i].IsMatch(Arg.Any<string>()).Returns(i != falseIndex);
+
+			_sut.IsMatch("wat").ShouldBeFalse();
+		}
+
+		[Theory]
+		[MemberData(nameof(Indexes))]
+		public void Children_after_first_false_match_are_not_evaluated(int falseIndex)
+		{
+			for (int i = 0; i < _inners.Length; i++)
+				_inners[i].IsMatch(Arg.Any<string>()).Returns(i != falseIndex);
+
+			_sut.IsMatch("wat").ShouldBeFalse();
+
+			for (int i = falseIndex + 1; i < _inners.Length; i++)
+				_inners[i].DidNotReceive().IsMatch(Arg.Any<string>());
+		}
+	}
+}
---
+++ src/Ruler/Specifications/AllSpecification.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace Ruler.Specifications
+{
+	public class AllSpecification<T> : ISpecification<T>
+	{
+		private readonly ISpecification<T>[] _inner;
+
+		public AllSpecification(params ISpecification<T>[] inner) => _inner = inner;
+
+		public bool IsMatch(T input) => _inner.All(inner => inner.IsMatch(input));
+	}
+}
