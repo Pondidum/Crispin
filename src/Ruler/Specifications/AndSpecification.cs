2180ae9abb058406f690dff80355cee5d23d1098
andy <andydavies2020@gmail.com>
2018-05-10 20:32:02 +0300

    rules: andSpecification is a constrained allSpecification

--- src/Ruler/Specifications/AndSpecification.cs
+++ src/Ruler/Specifications/AndSpecification.cs
@@ -1,16 +1,10 @@
 ﻿namespace Ruler.Specifications
 {
-	public class AndSpecification<T> : ISpecification<T>
+	public class AndSpecification<T> : AllSpecification<T>
 	{
-		private readonly ISpecification<T> _left;
-		private readonly ISpecification<T> _right;
-
 		public AndSpecification(ISpecification<T> left, ISpecification<T> right)
+			: base(left, right)
 		{
-			_left = left;
-			_right = right;
 		}
-
-		public bool IsMatch(T input) => _left.IsMatch(input) && _right.IsMatch(input);
 	}
 }
