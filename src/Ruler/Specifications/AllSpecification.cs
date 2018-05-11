7f669d6ab0e80a6a87eeab42d4c387f87b57daae
andy <andydavies2020@gmail.com>
2018-05-11 22:02:32 +0300

    rules: allow allSpec and anySpec to be built from Ienumerable as well as array

--- src/Ruler/Specifications/AllSpecification.cs
+++ src/Ruler/Specifications/AllSpecification.cs
@@ -1,12 +1,14 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Ruler.Specifications
 {
 	public class AllSpecification<T> : ISpecification<T>
 	{
-		private readonly ISpecification<T>[] _inner;
+		private readonly IEnumerable<ISpecification<T>> _inner;
 
 		public AllSpecification(params ISpecification<T>[] inner) => _inner = inner;
+		public AllSpecification(IEnumerable<ISpecification<T>> inner) => _inner = inner;
 
 		public bool IsMatch(T input) => _inner.All(inner => inner.IsMatch(input));
 	}
--- src/Ruler/Specifications/AnySpecification.cs
+++ src/Ruler/Specifications/AnySpecification.cs
@@ -1,12 +1,14 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Ruler.Specifications
 {
 	public class AnySpecification<T> : ISpecification<T>
 	{
-		private readonly ISpecification<T>[] _inner;
+		private readonly IEnumerable<ISpecification<T>> _inner;
 
 		public AnySpecification(params ISpecification<T>[] inner) => _inner = inner;
+		public AnySpecification(IEnumerable<ISpecification<T>> inner) => _inner = inner;
 
 		public bool IsMatch(T input) => _inner.Any(inner => inner.IsMatch(input));
 	}
