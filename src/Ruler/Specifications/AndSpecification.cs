57f07a4540b1a56506506762292654cc7a3b3d9e
andy <andydavies2020@gmail.com>
2018-05-11 22:01:53 +0300

    rules: allow AndSpec and OrSpec to be built from ienumerable<spec>

--- src/Ruler/Specifications/AndSpecification.cs
+++ src/Ruler/Specifications/AndSpecification.cs
@@ -1,7 +1,15 @@
-﻿namespace Ruler.Specifications
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Ruler.Specifications
 {
 	public class AndSpecification<T> : AllSpecification<T>
 	{
+		public AndSpecification(IEnumerable<ISpecification<T>> sequence)
+			: base(sequence.Take(2))
+		{
+		}
+
 		public AndSpecification(ISpecification<T> left, ISpecification<T> right)
 			: base(left, right)
 		{
--- src/Ruler/Specifications/OrSpecification.cs
+++ src/Ruler/Specifications/OrSpecification.cs
@@ -1,7 +1,15 @@
-﻿namespace Ruler.Specifications
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Ruler.Specifications
 {
 	public class OrSpecification<T> : AnySpecification<T>
 	{
+		public OrSpecification(IEnumerable<ISpecification<T>> sequence)
+			: base(sequence.Take(2))
+		{
+		}
+
 		public OrSpecification(ISpecification<T> left, ISpecification<T> right)
 			: base(left, right)
 		{
