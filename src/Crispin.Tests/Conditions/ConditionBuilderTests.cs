26994cb8a7d45a49f9f3bd816caf27e7023d1304
andy <andydavies2020@gmail.com>
2018-06-30 15:07:23 +0300

    conditionBuilder: tdd an implementation

---
+++ src/Crispin.Tests/Conditions/ConditionBuilderTests.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Crispin.Conditions;
+using Crispin.Conditions.ConditionTypes;
+using Shouldly;
+using Xunit;
+
+namespace Crispin.Tests.Conditions
+{
+	public class ConditionBuilderTests
+	{
+		private readonly ConditionBuilder _builder;
+		private readonly Dictionary<string, object> _props;
+
+		public ConditionBuilderTests()
+		{
+			_builder = new ConditionBuilder();
+			_props = new Dictionary<string, object>();
+		}
+
+		[Fact]
+		public void When_the_type_is_not_specified()
+		{
+			Should
+				.Throw<ConditionException>(() => _builder.CreateCondition(_props))
+				.Message.ShouldContain("Type was not specified");
+		}
+
+		public static IEnumerable<object[]> KnownConditions => typeof(Condition)
+			.Assembly.GetExportedTypes()
+			.Where(t => t.IsAbstract == false)
+			.Where(t => typeof(Condition).IsAssignableFrom(t))
+			.Select(t => t.Name.Replace("Condition", ""))
+			.Select(name => new[] { name });
+
+		[Theory]
+		[MemberData(nameof(KnownConditions))]
+		public void When_the_type_is_a_known_condition(string name)
+		{
+			_props["type"] = name;
+
+			_builder
+				.CreateCondition(_props)
+				.GetType().Name.ShouldStartWith(name, Case.Insensitive);
+		}
+
+		[Fact]
+		public void When_the_type_is_an_unknown_condition()
+		{
+			_props["type"] = "whaaaaaaat?!";
+
+			Should
+				.Throw<ConditionException>(() => _builder.CreateCondition(_props))
+				.Message.ShouldBe($"Unknown condition type '{_props["type"]}'");
+		}
+
+		[Fact]
+		public void When_creating_a_condition_with_extra_settings()
+		{
+			_props["type"] = "ingroup";
+			_props["searchKey"] = "needle";
+			_props["groupName"] = "haystack";
+
+			var condition = _builder
+				.CreateCondition(_props)
+				.ShouldBeOfType<InGroupCondition>();
+
+			condition.ShouldSatisfyAllConditions(
+				() => condition.GroupName.ShouldBe("haystack"),
+				() => condition.SearchKey.ShouldBe("needle")
+			);
+		}
+	}
+}
---
+++ src/Crispin/Conditions/ConditionBuilder.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Crispin.Conditions.ConditionTypes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Crispin.Conditions
+{
+	public class ConditionBuilder : IConditionBuilder
+	{
+		private static readonly Lazy<Dictionary<string, Func<Dictionary<string, object>, Condition>>> Conditions;
+
+		static ConditionBuilder()
+		{
+			Conditions = new Lazy<Dictionary<string, Func<Dictionary<string, object>, Condition>>>(BuildConditionsMap);
+		}
+
+		public Condition CreateCondition(Dictionary<string, object> conditionProperties)
+		{
+			if (conditionProperties.TryGetValue("type", out var type) == false)
+				throw new ConditionException("The Type was not specified");
+
+			if (Conditions.Value.TryGetValue(Convert.ToString(type), out var create) == false)
+				throw new ConditionException($"Unknown condition type '{type}'");
+
+			return create(conditionProperties);
+		}
+
+
+		private static Dictionary<string, Func<Dictionary<string, object>, Condition>> BuildConditionsMap()
+		{
+			var condition = typeof(Condition);
+
+			return condition
+				.Assembly.GetExportedTypes()
+				.Where(t => t.IsAbstract == false)
+				.Where(t => condition.IsAssignableFrom(t))
+				.ToDictionary(
+					t => t.Name.Replace("Condition", ""),
+					t => new Func<Dictionary<string, object>, Condition>(props => Deserialize(t, props)),
+					StringComparer.OrdinalIgnoreCase);
+		}
+
+		private static Condition Deserialize(Type type, Dictionary<string, object> props) =>
+			JObject
+				.FromObject(props)
+				.ToObject(type) as Condition;
+	}
+}
