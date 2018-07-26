ed8e13cc6205124e6ad33ba3975d3f0ca888897c
andy <andydavies2020@gmail.com>
2018-07-26 22:39:45 +0300

    projection: remove catchall functionality

--- src/Crispin.Tests/Infrastructure/ProjectionTests.cs
+++ src/Crispin.Tests/Infrastructure/ProjectionTests.cs
@@ -2,29 +2,11 @@
 using Crispin.Infrastructure;
 using Shouldly;
 using Xunit;
-using Xunit.Sdk;
 
 namespace Crispin.Tests.Infrastructure
 {
 	public class ProjectionTests
 	{
-		[Fact]
-		public void When_consuming_all_events()
-		{
-			var first = new FirstEvent();
-			var second = new SecondEvent();
-
-			var projection = new CatchAllProjection();
-			projection.Consume(first);
-			projection.Consume(second);
-
-			projection.SeenEvents.ShouldBe(new Event[]
-			{
-				first,
-				second
-			});
-		}
-
 		[Fact]
 		public void When_consuming_specific_events()
 		{
@@ -46,26 +28,6 @@ namespace Crispin.Tests.Infrastructure
 		public class FirstEvent : Event {}
 		public class SecondEvent : Event {}
 		public class ThirdEvent : Event {}
-
-		private class CatchAllProjection : Projection<Memento>
-		{
-			public List<Event> SeenEvents { get; }
-
-			public CatchAllProjection()
-			{
-				SeenEvents = new List<Event>();
-				RegisterAll(SeenEvents.Add);
-			}
-
-			protected override Memento CreateMemento()
-			{
-				return new Memento();
-			}
-
-			protected override void ApplyMemento(Memento memento)
-			{
-			}
-		}
 
 		private class TestProjection : Projection<Memento>
 		{
--- src/Crispin/Infrastructure/Projection.cs
+++ src/Crispin/Infrastructure/Projection.cs
@@ -6,23 +6,18 @@ namespace Crispin.Infrastructure
 	public abstract class Projection<TMemento> : IProjection
 	{
 		private readonly Dictionary<Type, Action<object>> _handlers;
-		private readonly List<Action<Event>> _catchAll;
 
 		protected Projection()
 		{
 			_handlers = new Dictionary<Type, Action<object>>();
-			_catchAll = new List<Action<Event>>();
 		}
 
 		protected void Register<TEvent>(Action<TEvent> handler) => _handlers.Add(typeof(TEvent), e => handler((TEvent)e));
-		protected void RegisterAll(Action<Event> handler) => _catchAll.Add(handler);
 
 		public void Consume(Event @event)
 		{
 			if (_handlers.TryGetValue(@event.GetType(), out var apply))
 				apply(@event);
-
-			_catchAll.ForEach(handle => handle(@event));
 		}
 
 		public object ToMemento() => CreateMemento();
