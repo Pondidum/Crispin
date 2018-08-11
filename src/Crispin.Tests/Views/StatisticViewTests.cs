8eb05ed6dd87f0ed8f403971a9ad7ddf38f53733
andy <andydavies2020@gmail.com>
2018-08-11 20:07:15 +0300

    statisticView: support last queried|active|inactive

--- src/Crispin.Tests/Views/StatisticViewTests.cs
+++ src/Crispin.Tests/Views/StatisticViewTests.cs
@@ -13,56 +13,86 @@ namespace Crispin.Tests.Views
 	{
 		private readonly StatisticView _view;
 		private readonly UserID _user;
+		private readonly DateTime _now;
 
 		public StatisticViewTests()
 		{
 			_view = new StatisticView();
 			_user = UserID.Parse("editor");
+			_now = DateTime.Now;
 		}
 
 		private void Apply(bool active, DateTime when = default(DateTime)) => _view.Apply(new StatisticReceived(
 			_user,
-			when == default(DateTime) ? DateTime.Now : when,
+			when == default(DateTime) ? _now : when,
 			active: active,
 			conditionStates: new Dictionary<ConditionID, bool>()
 		));
 
 		[Fact]
-		public void Adding_an_inactive_statistic_increases_active_count()
+		public void Adding_an_inactive_statistic()
 		{
 			Apply(active: false);
 
 			_view.ShouldSatisfyAllConditions(
 				() => _view.TotalQueries.ShouldBe(1),
 				() => _view.ActiveQueries.ShouldBe(0),
-				() => _view.ActivePercentage.ShouldBe(0)
+				() => _view.ActivePercentage.ShouldBe(0),
+				() => _view.LastQueried.ShouldBe(_now),
+				() => _view.LastInactive.ShouldBe(_now),
+				() => _view.LastActive.ShouldBe(DateTime.MinValue)
 			);
 		}
 
 		[Fact]
-		public void Adding_an_active_statistic_doesnt_affect_active_count()
+		public void Adding_an_active_statistic()
 		{
 			Apply(active: true);
 
 			_view.ShouldSatisfyAllConditions(
 				() => _view.TotalQueries.ShouldBe(1),
 				() => _view.ActiveQueries.ShouldBe(1),
-				() => _view.ActivePercentage.ShouldBe(100)
+				() => _view.ActivePercentage.ShouldBe(100),
+				() => _view.LastQueried.ShouldBe(_now),
+				() => _view.LastInactive.ShouldBe(DateTime.MinValue),
+				() => _view.LastActive.ShouldBe(_now)
 			);
 		}
 
 		[Fact]
 		public void Adding_multiple_adjusts_percentage()
 		{
-			Apply(active: true);
-			Apply(active: true);
-			Apply(active: false);
-			Apply(active: true);
+			Apply(active: true, when: _now);
+			Apply(active: true, when: _now.AddSeconds(1));
+			Apply(active: false, when: _now.AddSeconds(2));
+			Apply(active: true, when: _now.AddSeconds(3));
 
 			_view.ShouldSatisfyAllConditions(
 				() => _view.TotalQueries.ShouldBe(4),
 				() => _view.ActiveQueries.ShouldBe(3),
-				() => _view.ActivePercentage.ShouldBe(75)
+				() => _view.ActivePercentage.ShouldBe(75),
+				() => _view.LastQueried.ShouldBe(_now.AddSeconds(3)),
+				() => _view.LastActive.ShouldBe(_now.AddSeconds(3)),
+				() => _view.LastInactive.ShouldBe(_now.AddSeconds(2))
+			);
+		}
+
+
+		[Fact]
+		public void When_events_are_received_out_of_order()
+		{
+			Apply(active: true, when: _now);
+			Apply(active: true, when: _now.AddSeconds(7));
+			Apply(active: false, when: _now.AddSeconds(4));
+			Apply(active: true, when: _now.AddSeconds(5));
+
+			_view.ShouldSatisfyAllConditions(
+				() => _view.TotalQueries.ShouldBe(4),
+				() => _view.ActiveQueries.ShouldBe(3),
+				() => _view.ActivePercentage.ShouldBe(75),
+				() => _view.LastQueried.ShouldBe(_now.AddSeconds(7)),
+				() => _view.LastActive.ShouldBe(_now.AddSeconds(7)),
+				() => _view.LastInactive.ShouldBe(_now.AddSeconds(4))
 			);
 		}
 	}
--- src/Crispin/Views/StatisticView.cs
+++ src/Crispin/Views/StatisticView.cs
@@ -1,9 +1,14 @@
+using System;
 using Crispin.Events;
 
 namespace Crispin.Views
 {
 	public class StatisticView
 	{
+		public DateTime LastQueried { get; set; }
+		public DateTime LastActive { get; set; }
+		public DateTime LastInactive { get; set; }
+
 		public int ActivePercentage { get; set; }
 		public int TotalQueries { get; set; }
 		public int ActiveQueries { get; set; }
@@ -16,7 +21,15 @@ namespace Crispin.Views
 				ActiveQueries++;
 
 			ActivePercentage = (int)((100M / TotalQueries) * ActiveQueries);
+
+			LastQueried = Latest(LastQueried, @event.Timestamp);
+
+			if (@event.Active)
+				LastActive = Latest(LastActive, @event.Timestamp);
+			else
+				LastInactive = Latest(LastInactive, @event.Timestamp);
 		}
+
+		private static DateTime Latest(DateTime one, DateTime two) => one >= two ? one : two;
 	}
 }
-
