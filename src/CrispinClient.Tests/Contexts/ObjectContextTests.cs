01fd8ac501ba9f17ff218d1514b203a86c9af7fa
andy <andydavies2020@gmail.com>
2018-09-14 21:50:04 +0300

    client: refactor objectContext, test for null values

--- src/CrispinClient.Tests/Contexts/ObjectContextTests.cs
+++ src/CrispinClient.Tests/Contexts/ObjectContextTests.cs
@@ -8,51 +8,62 @@ namespace CrispinClient.Tests.Contexts
 {
 	public class ObjectContextTests
 	{
+		private const string TypeName = nameof(TestTarget);
+		private const string GoodGroup = nameof(TestTarget.SomeGroup);
+		private const string BadGroup = nameof(TestTarget.SomeBadGroup);
+		private const string NullGroup = nameof(TestTarget.NullGroup);
+		private const string InvalidGroup = "wat";
+
+		private readonly ObjectContext _context;
+
+		public ObjectContextTests()
+		{
+			_context = new ObjectContext(new TestTarget
+			{
+				SomeGroup = new HashSet<string> { "first", "second", "third" },
+				SomeBadGroup = new Uri("http://localhost")
+			});
+		}
+
 		[Theory]
 		[InlineData("second", true)]
 		[InlineData("fifth", false)]
 		public void GroupName_is_mapped_to_a_property(string searchTerm, bool found)
 		{
-			var context = new ObjectContext(new TestTarget
-			{
-				SomeGroup = new HashSet<string> { "first", "second", "third" }
-			});
-
-			context
-				.GroupContains(nameof(TestTarget.SomeGroup), searchTerm)
+			_context
+				.GroupContains(GoodGroup, searchTerm)
 				.ShouldBe(found);
 		}
 
 		[Fact]
 		public void When_a_groupName_doesnt_match_a_property()
 		{
-			var context = new ObjectContext(new TestTarget
-			{
-				SomeGroup = new HashSet<string> { "first", "second", "third" }
-			});
-
 			Should
-				.Throw<MissingMemberException>(() => context.GroupContains("wat", "is this"))
-				.Message.ShouldBe($"Member '{nameof(TestTarget)}.wat' not found.");
+				.Throw<MissingMemberException>(() => _context.GroupContains(InvalidGroup, "is this"))
+				.Message.ShouldBe($"Member '{TypeName}.{InvalidGroup}' not found.");
+		}
+
+		[Fact]
+		public void When_the_property_is_null()
+		{
+			Should
+				.Throw<NullReferenceException>(() => _context.GroupContains(NullGroup, "is this"))
+				.Message.ShouldBe($"Member '{TypeName}.{NullGroup}' is null.");
 		}
 
 		[Fact]
 		public void When_the_property_is_not_enumerable_string()
 		{
-			var context = new ObjectContext(new TestTarget
-			{
-				SomeGroup = new HashSet<string> { "first", "second", "third" },
-				SomeBadGroup = new Uri("http://localhost")
-			});
-
 			Should
-				.Throw<InvalidCastException>(() => context.GroupContains(nameof(TestTarget.SomeBadGroup), "is this"))
-				.Message.ShouldBe($"Member '{nameof(TestTarget)}.{nameof(TestTarget.SomeBadGroup)}' does not implement IEnumerable<string>.");
+				.Throw<InvalidCastException>(() => _context.GroupContains(BadGroup, "is this"))
+				.Message.ShouldBe($"Member '{TypeName}.{BadGroup}' does not implement IEnumerable<string>.");
 		}
+
 
 		private class TestTarget
 		{
 			public HashSet<string> SomeGroup { get; set; }
+			public HashSet<string> NullGroup { get; set; }
 			public Uri SomeBadGroup { get; set; }
 		}
 	}
--- src/CrispinClient/Contexts/ObjectContext.cs
+++ src/CrispinClient/Contexts/ObjectContext.cs
@@ -7,6 +7,12 @@ namespace CrispinClient.Contexts
 {
 	public class ObjectContext : IToggleContext
 	{
+		private const BindingFlags PropertyFlags =
+			BindingFlags.Public |
+			BindingFlags.NonPublic |
+			BindingFlags.Instance |
+			BindingFlags.IgnoreCase;
+
 		private readonly object _target;
 
 		public ObjectContext(object target)
@@ -16,17 +22,18 @@ namespace CrispinClient.Contexts
 
 		public bool GroupContains(string groupName, string term)
 		{
-			var propertyFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.IgnoreCase;
-
-			var group = _target.GetType().GetProperty(groupName, propertyFlags);
+			var group = _target.GetType().GetProperty(groupName, PropertyFlags);
 
 			if (group == null)
 				throw new MissingMemberException(_target.GetType().Name, groupName);
 
-			var searchable = group.GetValue(_target) as IEnumerable<string>;
+			if (typeof(IEnumerable<string>).IsAssignableFrom(group.PropertyType) == false)
+				throw new InvalidCastException($"Member '{_target.GetType().Name}.{group.Name}' does not implement IEnumerable<string>.");
+
+			var searchable = (IEnumerable<string>)group.GetValue(_target);
 
 			if (searchable == null)
-				throw new InvalidCastException($"Member '{_target.GetType().Name}.{group.Name}' does not implement IEnumerable<string>.");
+				throw new NullReferenceException($"Member '{_target.GetType().Name}.{group.Name}' is null.");
 
 			return searchable.Contains(term);
 		}
