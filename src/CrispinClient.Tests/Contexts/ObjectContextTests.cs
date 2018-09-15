09f921abaa97bbc6ba668b95e4b2c722212b7474
andy <andydavies2020@gmail.com>
2018-09-15 13:37:38 +0300

    clinet: implement method invokation too

--- src/CrispinClient.Tests/Contexts/ObjectContextTests.cs
+++ src/CrispinClient.Tests/Contexts/ObjectContextTests.cs
@@ -25,16 +25,6 @@ namespace CrispinClient.Tests.Contexts
 			});
 		}
 
-		[Theory]
-		[InlineData("second", true)]
-		[InlineData("fifth", false)]
-		public void GroupName_is_mapped_to_a_property(string searchTerm, bool found)
-		{
-			_context
-				.GroupContains(GoodGroup, searchTerm)
-				.ShouldBe(found);
-		}
-
 		[Fact]
 		public void When_a_groupName_doesnt_match_a_property()
 		{
@@ -59,12 +49,68 @@ namespace CrispinClient.Tests.Contexts
 				.Message.ShouldBe($"Member '{TypeName}.{BadGroup}' does not implement IEnumerable<string>.");
 		}
 
+		[Theory]
+		[InlineData(nameof(TestTarget.SomeGroup), "second", true)]
+		[InlineData(nameof(TestTarget.SomeGroup), "fifth", false)]
+		[InlineData(nameof(TestTarget.FunctionGroup), "second", true)]
+		[InlineData(nameof(TestTarget.FunctionGroup), "fifth", false)]
+		[InlineData(nameof(TestTarget.Overloaded), "second", true)]
+		[InlineData(nameof(TestTarget.Overloaded), "fifth", false)]
+		public void GroupName_is_mapped_to_a_function_or_property(string groupName, string searchTerm, bool found)
+		{
+			_context
+				.GroupContains(groupName, searchTerm)
+				.ShouldBe(found);
+		}
+
+		[Theory]
+		[InlineData(nameof(TestTarget.BadReturn))]
+		[InlineData(nameof(TestTarget.BadParameter))]
+		[InlineData(nameof(TestTarget.MultipleParameters))]
+		public void Matching_methods_should_be_found(string functionName)
+		{
+			Should
+				.Throw<MissingMethodException>(() => _context.GroupContains(functionName, "is this"))
+				.Message.ShouldBe($"No method found with a signature 'bool {TypeName}.{functionName}(string term)' (case insensitive).");
+		}
+
+		[Fact]
+		public void Overloaded_methods_with_no_matching_method()
+		{
+			var functionName = nameof(TestTarget.BadOverload);
+
+			var message = Should
+				.Throw<MissingMethodException>(() => _context.GroupContains(functionName, "is this"))
+				.Message;
+
+			message.ShouldSatisfyAllConditions(
+				() => message.ShouldStartWith($"No method found with a signature 'bool {TypeName}.{functionName}(string term)' (case insensitive)."),
+				() => message.ShouldContain("Found:"),
+				() => message.ShouldContain($"* Boolean {TypeName}.{functionName}(Uri one)"),
+				() => message.ShouldContain($"* Boolean {TypeName}.{functionName}(String one, String two)"),
+				() => message.ShouldContain($"* Void {TypeName}.{functionName}()")
+			);
+		}
 
 		private class TestTarget
 		{
 			public HashSet<string> SomeGroup { get; set; }
 			public HashSet<string> NullGroup { get; set; }
 			public Uri SomeBadGroup { get; set; }
+
+			public bool FunctionGroup(string term) => SomeGroup.Contains(term);
+			public Uri BadReturn(string term) => new Uri("http://localhost");
+			public bool BadParameter(Uri term) => false;
+			public bool MultipleParameters(string one, string two) => false;
+
+			public bool Overloaded(string term) => SomeGroup.Contains(term);
+			public bool Overloaded(string one, string two) => true;
+
+			public bool BadOverload(Uri one) => true;
+			public bool BadOverload(string one, string two) => true;
+			public void BadOverload()
+			{
+			}
 		}
 	}
 }
--- src/CrispinClient/Contexts/ObjectContext.cs
+++ src/CrispinClient/Contexts/ObjectContext.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 
 namespace CrispinClient.Contexts
 {
@@ -22,20 +23,72 @@ namespace CrispinClient.Contexts
 
 		public bool GroupContains(string groupName, string term)
 		{
-			var group = _target.GetType().GetProperty(groupName, PropertyFlags);
+			var methods = _target.GetType()
+				.GetMethods(PropertyFlags)
+				.Where(m => m.Name.Equals(groupName, StringComparison.OrdinalIgnoreCase))
+				.ToArray();
 
-			if (group == null)
+			if (methods.Any())
+				return RunMethod(methods, groupName, term);
+
+			var property = _target.GetType().GetProperty(groupName, PropertyFlags);
+
+			return RunProperty(property, groupName, term);
+		}
+
+		private bool RunProperty(PropertyInfo property, string groupName, string term)
+		{
+			if (property == null)
 				throw new MissingMemberException(_target.GetType().Name, groupName);
 
-			if (typeof(IEnumerable<string>).IsAssignableFrom(group.PropertyType) == false)
-				throw new InvalidCastException($"Member '{_target.GetType().Name}.{group.Name}' does not implement IEnumerable<string>.");
+			if (typeof(IEnumerable<string>).IsAssignableFrom(property.PropertyType) == false)
+				throw new InvalidCastException($"Member '{_target.GetType().Name}.{property.Name}' does not implement IEnumerable<string>.");
 
-			var searchable = (IEnumerable<string>)group.GetValue(_target);
+			var searchable = (IEnumerable<string>)property.GetValue(_target);
 
 			if (searchable == null)
-				throw new NullReferenceException($"Member '{_target.GetType().Name}.{group.Name}' is null.");
+				throw new NullReferenceException($"Member '{_target.GetType().Name}.{property.Name}' is null.");
 
 			return searchable.Contains(term);
+		}
+
+		private bool RunMethod(MethodInfo[] methods, string groupName, string term)
+		{
+			var matches = methods
+				.Where(m => m.ReturnType == typeof(bool))
+				.Where(m => m.GetParameters().Length == 1)
+				.Where(m => m.GetParameters().Single().ParameterType == typeof(string))
+				.ToArray();
+
+			if (matches.Length != 1)
+			{
+				var message = $"No method found with a signature 'bool {_target.GetType().Name}.{groupName}(string term)' (case insensitive).";
+
+				if (methods.Length > 1)
+					message = message + BuildSignatures(methods);
+
+				throw new MissingMethodException(message);
+			}
+
+			return (bool)matches.Single().Invoke(_target, new object[] { term });
+		}
+
+		private string BuildSignatures(MethodInfo[] methods)
+		{
+			var targetName = _target.GetType().Name;
+			
+			var sb = new StringBuilder();
+			sb.AppendLine();
+			sb.AppendLine("Found:");
+
+			foreach (var method in methods)
+			{
+				var parameters = method.GetParameters().Select(m => $"{m.ParameterType.Name} {m.Name}");
+				
+				sb.AppendLine($"* {method.ReturnType.Name} {targetName}.{method.Name}({string.Join(", ", parameters)})");
+			}
+
+			return sb.ToString();
 		}
 	}
 }
