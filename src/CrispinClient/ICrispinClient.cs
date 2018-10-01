5875639b0d6b76be2396b07bf7690f1d0659c17d
andy <andydavies2020@gmail.com>
2018-10-01 22:51:34 +0300

    client: move client and statistic dto to root so they can be shared

--- src/CrispinClient/ICrispinClient.cs
+++ src/CrispinClient/ICrispinClient.cs
@@ -1,9 +1,10 @@
 using System.Collections.Generic;
 
-namespace CrispinClient.Fetching
+namespace CrispinClient
 {
 	public interface ICrispinClient
 	{
 		IEnumerable<Toggle> GetAllToggles();
+		void SendStatistics(IEnumerable<Statistic> statistics);
 	}
 }
--- src/CrispinClient/Statistic.cs
+++ src/CrispinClient/Statistic.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 
-namespace CrispinClient.Statistics
+namespace CrispinClient
 {
 	public class Statistic
 	{
