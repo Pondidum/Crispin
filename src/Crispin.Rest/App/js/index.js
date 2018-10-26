ec9eaf0215cbf3c82f565164fc748677d63577d8
andy <andydavies2020@gmail.com>
2018-10-26 21:53:39 +0300

    ui: add reactstrap and bootstrap

--- src/Crispin.Rest/App/js/index.js
+++ src/Crispin.Rest/App/js/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import AppRouter from "./router";
 
 ReactDom.render(<AppRouter />, document.getElementById("container"));
