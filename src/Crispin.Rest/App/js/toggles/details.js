9f1dbc483440aee9536a25d1521a3bb58c426872
andy <andydavies2020@gmail.com>
2018-10-30 17:41:43 +0200

    ui: simplify toggle name,desc,conditions layout

--- src/Crispin.Rest/App/js/toggles/details.js
+++ src/Crispin.Rest/App/js/toggles/details.js
@@ -1,55 +1,51 @@
 import React from "react";
-import { Row, Col, CardColumns } from "reactstrap";
+import { Row, Col } from "reactstrap";
 
 import ToggleGraph from "./graph";
 
-const headerWidth = 3;
-const detailsWidth = 12 - headerWidth;
-
 const Details = ({ match }) => (
   <Row>
-    <Col md="5">
-      <Row>
-        <Col md={headerWidth}>Name</Col>
-        <Col md={detailsWidth}>Some Feature Toggle {match.params.id}</Col>
-      </Row>
-      <Row>
-        <Col md={headerWidth}>Description</Col>
-        <Col md={detailsWidth}>
-          does something very interesting and potentially has quite a long
+    <Col md="6">
+      <div>
+        <h4>Name</h4>
+        <p>Some Feature Toggle {match.params.id}</p>
+      </div>
+      <div>
+        <h4>Description</h4>
+        <p>
+          Does something very interesting and potentially has quite a long
           description
-        </Col>
-      </Row>
-      <Row>
-        <Col md={headerWidth}>Conditions</Col>
-        <Col md={detailsWidth}>
+        </p>
+      </div>
+      <div>
+        <h4>Conditions</h4>
+        <p>
           Toggle is active when <b>any</b> | <a href="#">all</a> conditions are
           true.
-        </Col>
-        <Col md={{ size: detailsWidth, offset: headerWidth }}>
-          <ul>
-            <li>
-              When user is in group <b>Alpha Testers</b>
-            </li>
-            <li>
-              When user is in group <b>Beta Testers</b>
-            </li>
-            <li>
-              When All are True
-              <ul>
-                <li>
-                  When user is in group <b>Canary</b>
-                </li>
-                <li>
-                  When user is in <b>5%</b> of users
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </Col>
-      </Row>
+        </p>
+
+        <ul>
+          <li>
+            When user is in group <b>Alpha Testers</b>
+          </li>
+          <li>
+            When user is in group <b>Beta Testers</b>
+          </li>
+          <li>
+            When All are True
+            <ul>
+              <li>
+                When user is in group <b>Canary</b>
+              </li>
+              <li>
+                When user is in <b>5%</b> of users
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </Col>
-    <Col md="7">
+    <Col md="6">
       <ToggleGraph title="Usage Graph" />
       <ToggleGraph title="Activation Graph" />
       <ToggleGraph title="Condition Activation Graph" />
