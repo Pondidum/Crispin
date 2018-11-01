f2da0ced4d948acac003763056b5bca3637ad30b
andy <andydavies2020@gmail.com>
2018-11-01 22:47:34 +0200

    ui: fix trailing slashes, as react-router can't?

--- src/Crispin.Rest/App/js/toggles/menu-entry.js
+++ src/Crispin.Rest/App/js/toggles/menu-entry.js
@@ -2,9 +2,12 @@ import React from "react";
 import { NavItem } from "reactstrap";
 import { NavLink } from "react-router-dom";
 
+const combine = (left, right) =>
+  left.endsWith("/") ? left + right : left + "/" + right;
+
 const ToggleMenuEntry = ({ match, id }) => (
   <NavItem>
-    <NavLink className="nav-link" to={`${match.url}/${id}`}>
+    <NavLink className="nav-link" to={combine(match.url, id)}>
       Toggle {id}
     </NavLink>
   </NavItem>
