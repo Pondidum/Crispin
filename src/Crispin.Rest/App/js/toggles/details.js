b78f3f886cdbdd520f7cc33bdada8279c149dda0
andy <andydavies2020@gmail.com>
2018-11-09 22:12:36 +0200

    ui: hook up name updating to toggle details

--- src/Crispin.Rest/App/js/toggles/details.js
+++ src/Crispin.Rest/App/js/toggles/details.js
@@ -6,6 +6,8 @@ import ToggleGraph from "./graph";
 import TextEditor from "./text-editor";
 import ConditionEditor from "./condition-editor";
 
+import { updateName } from "./actions";
+
 const mapStateToProps = (state, ownProps) => {
   const match = ownProps.match;
   return {
@@ -13,21 +15,39 @@ const mapStateToProps = (state, ownProps) => {
   };
 };
 
-const Protect = ({ toggle }) => (toggle ? <Details toggle={toggle} /> : null);
+const mapDispatchToProps = dispatch => {
+  return {
+    updateName: (id, newName) => dispatch(updateName(id, newName))
+  };
+};
 
-const Details = ({ toggle }) => (
-  <Row>
-    <Col md="6">
-      <TextEditor title="Name" value={toggle.name} />
-      <TextEditor title="Description" value={toggle.description} />
-      <ConditionEditor />
-    </Col>
-    <Col md="6">
-      <ToggleGraph title="Usage Graph" />
-      <ToggleGraph title="Activation Graph" />
-      <ToggleGraph title="Condition Activation Graph" />
-    </Col>
-  </Row>
+const connector = connect(
+  mapStateToProps,
+  mapDispatchToProps
 );
 
-export default connect(mapStateToProps)(Protect);
+const Details = ({ toggle, updateName }) => {
+  if (!toggle) {
+    return <div />;
+  }
+  return (
+    <Row>
+      <Col md="6">
+        <TextEditor
+          title="Name"
+          value={toggle.name}
+          onAcceptEdit={value => updateName(toggle.id, value)}
+        />
+        <TextEditor title="Description" value={toggle.description} />
+        <ConditionEditor />
+      </Col>
+      <Col md="6">
+        <ToggleGraph title="Usage Graph" />
+        <ToggleGraph title="Activation Graph" />
+        <ToggleGraph title="Condition Activation Graph" />
+      </Col>
+    </Row>
+  );
+};
+
+export default connector(Details);
