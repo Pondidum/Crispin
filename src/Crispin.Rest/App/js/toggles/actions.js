c29ae1a95914bc8ea4bedf00e21aa2e99fc0a188
andy <andydavies2020@gmail.com>
2018-11-09 22:05:45 +0200

    ui: implement actions and reducers for updating toggle name

--- src/Crispin.Rest/App/js/toggles/actions.js
+++ src/Crispin.Rest/App/js/toggles/actions.js
@@ -1,5 +1,7 @@
 export const ALL_TOGGLES_REQUESTED = "ALL_TOGGLES_REQUESTED";
 export const ALL_TOGGLES_RECEIVED = "ALL_TOGGLES_RECEIVED";
+export const UPDATE_TOGGLE_NAME_REQUESTED = "UPDATE_TOGGLE_NAME_REQUESTED";
+export const UPDATE_TOGGLE_NAME_COMPLETED = "UPDATE_TOGGLE_NAME_COMPLETED";
 
 export const allTogglesRequested = () => ({
   type: ALL_TOGGLES_REQUESTED
@@ -15,4 +17,23 @@ export const fetchAllToggles = () => dispatch => {
   return fetch(`/api/toggles`)
     .then(response => response.json())
     .then(json => dispatch(allTogglesReceived(json)));
+};
+
+export const updateToggleNameRequested = (toggleID, newName) => ({
+  type: UPDATE_TOGGLE_NAME_REQUESTED,
+  toggleID,
+  newName
+});
+
+export const toggleNameUpdated = (toggleID, newName) => ({
+  type: UPDATE_TOGGLE_NAME_COMPLETED,
+  toggleID,
+  newName
+});
+
+export const updateName = (toggleID, newName) => dispatch => {
+  dispatch(updateToggleNameRequested(toggleID, newName));
+  return fetch({ method: "PUT", url: `/api/toggles` })
+    .then(response => response.json())
+    .then(json => dispatch(toggleNameUpdated(json.id, json.name)));
 };
--- src/Crispin.Rest/App/js/toggles/reducers.js
+++ src/Crispin.Rest/App/js/toggles/reducers.js
@@ -1,8 +1,41 @@
-import { ALL_TOGGLES_REQUESTED, ALL_TOGGLES_RECEIVED } from "./actions";
+import {
+  ALL_TOGGLES_REQUESTED,
+  ALL_TOGGLES_RECEIVED,
+  UPDATE_TOGGLE_NAME_REQUESTED,
+  UPDATE_TOGGLE_NAME_COMPLETED
+} from "./actions";
 
 const DefaultState = {
   isFetching: false,
   all: []
+};
+
+const reduceToggle = (state, action) => {
+  switch (action.type) {
+    case UPDATE_TOGGLE_NAME_REQUESTED:
+      return { ...state, name: action.newName, updating: true };
+    case UPDATE_TOGGLE_NAME_COMPLETED:
+      return { ...state, name: action.newName, updating: false };
+    default:
+      return state;
+  }
+};
+
+const reduceArray = (state, action) => {
+  switch (action.type) {
+    case UPDATE_TOGGLE_NAME_REQUESTED:
+    case UPDATE_TOGGLE_NAME_COMPLETED: {
+      const index = state.findIndex(t => t.id === action.toggleID);
+      const newToggle = reduceToggle(state[index], action);
+
+      return Object.assign([], state, {
+        [index]: newToggle
+      });
+    }
+
+    default:
+      return state;
+  }
 };
 
 const reducer = (state = DefaultState, action) => {
@@ -19,6 +52,13 @@ const reducer = (state = DefaultState, action) => {
         fetching: false,
         all: action.toggles
       };
+
+    case UPDATE_TOGGLE_NAME_REQUESTED:
+      return {
+        ...state,
+        all: reduceArray(state.all, action)
+      };
+
     default:
       return state;
   }
