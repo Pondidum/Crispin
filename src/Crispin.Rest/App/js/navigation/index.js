4c6492ecad27bcfcb229012bc97b004903006b10
andy <andydavies2020@gmail.com>
2018-11-14 20:38:16 +0200

    ui: use glyphs for menu, add alt text to glyphs

--- src/Crispin.Rest/App/js/navigation/index.js
+++ src/Crispin.Rest/App/js/navigation/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Nav, NavItem } from "reactstrap";
 import { NavLink } from "react-router-dom";
+import Glyph from "../util/Glyph";
 
 const Navigation = () => (
   <nav className="col-md-1 navbar-dark bg-dark sidebar">
@@ -8,12 +9,12 @@ const Navigation = () => (
       <Nav className="ml-auto" className="navbar-nav" vertical>
         <NavItem>
           <NavLink className="nav-link" to="/toggles/">
-            Toggles
+            <Glyph name="toggle-on" alt="Toggles" />
           </NavLink>
         </NavItem>
         <NavItem>
           <NavLink className="nav-link" to="/users/">
-            Users
+            <Glyph name="users" alt="Users" />
           </NavLink>
         </NavItem>
       </Nav>
--- src/Crispin.Rest/App/js/util/glyph.js
+++ src/Crispin.Rest/App/js/util/glyph.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "@fortawesome/fontawesome-free/css/all.css";
 
-const Glyph = ({ name }) => <span class={"glyph fas fa-" + name} />;
+const Glyph = ({ name, alt }) => (
+  <span title={alt} class={"glyph fas fa-" + name} />
+);
 export default Glyph;
