da8a573ea202b72f7ae79562d8efd881ad18ee8c
andy <andydavies2020@gmail.com>
2018-11-15 18:01:38 +0200

    ui: fix class vs className in react props

--- src/Crispin.Rest/App/js/toggles/navigation.js
+++ src/Crispin.Rest/App/js/toggles/navigation.js
@@ -1,7 +1,10 @@
 import { connect } from "react-redux";
 import React from "react";
-import { Nav } from "reactstrap";
+import { Col, Nav } from "reactstrap";
 import MenuEntry from "./menu-entry";
+import Glyph from "../util/glyph";
+
+import { fetchAllToggles } from "./actions";
 
 const mapPropsFromState = (state, ownProps) => {
   return {
@@ -10,12 +13,37 @@ const mapPropsFromState = (state, ownProps) => {
   };
 };
 
-const Navigation = ({ match, toggles }) => (
-  <Nav vertical className="sidebar-sticky">
-    {toggles.map(t => (
-      <MenuEntry key={t.id} match={match} id={t.id} name={t.name} />
-    ))}
-  </Nav>
+const mapDispatchToProps = dispatch => ({
+  refresh: () => dispatch(fetchAllToggles())
+});
+
+const connector = connect(
+  mapPropsFromState,
+  mapDispatchToProps
 );
 
-export default connect(mapPropsFromState)(Navigation);
+const Navigation = ({ match, toggles, refresh }) => {
+  const handleRefresh = e => {
+    e.preventDefault();
+    refresh();
+  };
+
+  return (
+    <Col sm="3" md="2" className="sidebar">
+      <nav className="navbar navbar-expand navbar-dark bg-dark justify-content-end">
+        <div className="navbar-nav">
+          <a className="nav-item nav-link" href="#" onClick={handleRefresh}>
+            <Glyph name="sync" alt="Refresh toggles" />
+          </a>
+        </div>
+      </nav>
+      <Nav vertical className="sidebar-sticky">
+        {toggles.map(t => (
+          <MenuEntry key={t.id} match={match} id={t.id} name={t.name} />
+        ))}
+      </Nav>
+    </Col>
+  );
+};
+
+export default connector(Navigation);
--- src/Crispin.Rest/App/js/util/glyph.js
+++ src/Crispin.Rest/App/js/util/glyph.js
@@ -2,6 +2,6 @@ import React from "react";
 import "@fortawesome/fontawesome-free/css/all.css";
 
 const Glyph = ({ name, alt }) => (
-  <span title={alt} class={"glyph fas fa-" + name} />
+  <span title={alt} className={"glyph fas fa-" + name} />
 );
 export default Glyph;
