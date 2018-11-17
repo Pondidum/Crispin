1eecc78d2017e8f3f9eaf1cd16fb0d91f5a2382f
andy <andydavies2020@gmail.com>
2018-11-17 22:44:05 +0200

    ui: implement filter on toggle list

--- src/Crispin.Rest/App/js/toggles/toggle-list/index.js
+++ src/Crispin.Rest/App/js/toggles/toggle-list/index.js
@@ -1,6 +1,6 @@
 import { connect } from "react-redux";
-import React from "react";
-import { Col, Nav } from "reactstrap";
+import React, { Component } from "react";
+import { Col, Nav, Input } from "reactstrap";
 import MenuEntry from "./menu-entry";
 import Updating from "./updating";
 import Glyph from "../../util/glyph";
@@ -27,36 +27,72 @@ const connector = connect(
   mapDispatchToProps
 );
 
-const Navigation = ({ match, toggles, updating, refresh }) => {
-  const handleRefresh = e => {
-    e.preventDefault();
-    refresh();
-  };
+class Navigation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { filter: "" };
+  }
 
-  return (
-    <Col sm="3" md="2" className="sidebar">
-      <div className="toggle-list-header">
-        <nav className="navbar navbar-expand navbar-dark bg-dark justify-content-between ">
-          <div className="navbar-nav">
-            <a className="nav-item nav-link" href="#" onClick={handleRefresh}>
-              <Glyph name="plus" alt="Create new Toggle" />
-            </a>
-          </div>
-          <div className="navbar-nav">
-            <a className="nav-item nav-link" href="#" onClick={handleRefresh}>
-              <Glyph name="sync" alt="Refresh toggles" />
-            </a>
-          </div>
-        </nav>
-        <Updating updating={updating} />
-      </div>
-      <Nav vertical className="sidebar-sticky">
-        {toggles.map(t => (
-          <MenuEntry key={t.id} match={match} id={t.id} name={t.name} />
-        ))}
-      </Nav>
-    </Col>
-  );
-};
+  render() {
+    const handleRefresh = e => {
+      e.preventDefault();
+      this.props.refresh();
+    };
+
+    const filter = this.state.filter;
+    const filteredToggles = filter
+      ? this.props.toggles.filter(t => t.name.toLowerCase().includes(filter))
+      : this.props.toggles;
+
+    return (
+      <Col sm="3" md="2" className="sidebar">
+        <div className="toggle-list-header">
+          <nav className="navbar navbar-expand navbar-dark bg-dark justify-content-between ">
+            <div className="navbar-nav">
+              <a className="nav-item nav-link" href="#" onClick={handleRefresh}>
+                <Glyph name="plus" alt="Create new Toggle" />
+              </a>
+            </div>
+            <div className="navbar-nav">
+              <a className="nav-item nav-link" href="#" onClick={handleRefresh}>
+                <Glyph name="sync" alt="Refresh toggles" />
+              </a>
+            </div>
+          </nav>
+          <Updating updating={this.props.updating} />
+        </div>
+        <Input
+          bsSize="sm"
+          placeholder="filter..."
+          onChange={e =>
+            this.setState({ filter: e.target.value.toLowerCase() })
+          }
+        />
+        <Nav vertical className="sidebar-sticky">
+          {filteredToggles.map(t => (
+            <MenuEntry
+              key={t.id}
+              match={this.props.match}
+              id={t.id}
+              name={t.name}
+            />
+          ))}
+        </Nav>
+      </Col>
+    );
+  }
+}
+// const Navigation = ({ match, toggles, updating, refresh }) => {
+//   let filter; // = React.createRef();
+
+//   const filteredToggles = toggles.filter(t => {
+//     console.log(filter);
+//     return true;
+//   });
+
+//   const onChange = e => {
+//     filter = e.target.value.toLowerCase();
+//   };
+// };
 
 export default connector(Navigation);
