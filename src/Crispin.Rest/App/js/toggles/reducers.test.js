e638bb2c5b5ab2d92c893263b24e461f1d150c6e
andy <andydavies2020@gmail.com>
2018-11-20 17:58:05 +0200

    ui: add deep-freeze and start testing reducers

--- src/Crispin.Rest/App/js/toggles/reducers.test.js
+++ src/Crispin.Rest/App/js/toggles/reducers.test.js
@@ -1,4 +1,5 @@
 import reducer from "./reducers";
+import freeze from "deep-freeze";
 
 import {
   FETCH_ALL_TOGGLES_STARTED,
@@ -13,7 +14,25 @@ import {
 
 describe("fetching all toggles", () => {
   it("should mark the collection as updating when starting", () => {
-    const state = reducer({}, { type: FETCH_ALL_TOGGLES_STARTED });
+    const initialState = freeze({});
+    const state = reducer(initialState, { type: FETCH_ALL_TOGGLES_STARTED });
     expect(state.updating).toBe(true);
+  });
+
+  it("should mark the collection as no updating when finishing", () => {
+    const initialState = freeze({});
+    const state = reducer(initialState, { type: FETCH_ALL_TOGGLES_FINISHED });
+    expect(state.updating).toBe(false);
+  });
+
+  it("should replace the existing collection items with new", () => {
+    const initialState = freeze({ all: ["one", "two"] });
+    const event = freeze({
+      type: FETCH_ALL_TOGGLES_FINISHED,
+      toggles: ["three", "four", "five"]
+    });
+    const state = reducer(initialState, event);
+
+    expect(state.all).toEqual(["three", "four", "five"]);
   });
 });
