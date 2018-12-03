a6091b88f48ad77051ee8a682f2e37a47d6add7a
andy <andydavies2020@gmail.com>
2018-12-03 21:23:56 +0200

    ui: fetch system info on startup

--- src/Crispin.Rest/App/js/index.js
+++ src/Crispin.Rest/App/js/index.js
@@ -6,7 +6,9 @@ import { apiMiddleware } from "redux-api-middleware";
 
 import reducers from "./reducers";
 import AppRouter from "./router";
+
 import { fetchAllToggles } from "./toggles/actions";
+import { fetchSystemInfo } from "./system/actions";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../css/ui.css";
@@ -17,6 +19,8 @@ const store = createStore(
   reducers,
   composeEnhancers(applyMiddleware(thunk, apiMiddleware))
 );
+
 store.dispatch(fetchAllToggles());
+store.dispatch(fetchSystemInfo());
 
 ReactDom.render(<AppRouter store={store} />, document.getElementById("root"));
--- src/Crispin.Rest/App/js/reducers.js
+++ src/Crispin.Rest/App/js/reducers.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
+import system from "./system/reducers";
 import toggles from "./toggles/reducers";
 
 export default combineReducers({
-  toggles
+  toggles,
+  system
 });
